754bc1cdce004ce22ea9681446bca84636b382fa
Henry Doan <henrydoan96@gmail.com>
2021-03-22 21:14:37 -0600

    end of lecture

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
 
   # no longer for navigation, api call, api routes
   namespace :api do 
-    resources :todos 
-    # resources :todos do
-    #   resources :comments
-    # end
+    # resources :todos 
+    resources :todos do
+      resources :comments
+    end
   end
 
 end
